d1779230ffee13bb83c494b4efd076bbaf361cd4
ClausKlein <claus.klein@arcormail.de>
2024-03-27 23:06:23 +0100

    Check only public interface header, no details

--- src/tests/unit/read_until.cpp
+++ src/tests/unit/read_until.cpp
@@ -96,6 +96,8 @@ private:
   size_t next_read_length_;
 };
 
+// NOLINTBEGIN
+
 static const char read_data[]
   = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -1717,3 +1719,5 @@ ASIO_TEST_SUITE
   ASIO_TEST_CASE(test_dynamic_string_async_read_until_match_condition)
   ASIO_TEST_CASE(test_streambuf_async_read_until_match_condition)
 )
+
+// NOLINTEND
