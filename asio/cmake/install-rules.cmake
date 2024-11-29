99c90ef801e0830709a477e7a9981e260c99f5a0
ClausKlein <claus.klein@arcormail.de>
2024-11-29 19:11:39 +0100

    Test installed cmake config package
    Install cmake config package to <prefix>/lib
    Install files less verbose
    Add examples to cmake project
    Use debug postfix for install asio library

--- asio/cmake/install-rules.cmake
+++ asio/cmake/install-rules.cmake
@@ -1,6 +1,6 @@
-if(PROJECT_IS_TOP_LEVEL)
-  set(CMAKE_INSTALL_INCLUDEDIR "include/asio-${PROJECT_VERSION}" CACHE PATH "")
-endif()
+# if(PROJECT_IS_TOP_LEVEL)
+# NO!   set(CMAKE_INSTALL_INCLUDEDIR "include/asio-${PROJECT_VERSION}" CACHE PATH "")
+# endif()
 
 # Project is configured with no languages, so tell GNUInstallDirs the lib dir
 set(CMAKE_INSTALL_LIBDIR lib CACHE PATH "")
@@ -9,6 +9,9 @@ include(cmake/AddUninstallTarget.cmake)
 
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
+
+# Print Installing but not Up-to-date messages.
+set(CMAKE_INSTALL_MESSAGE LAZY)
 
 # find_package(<package) call for consumers to find this project
 set(_package asio)
@@ -25,7 +28,7 @@ endif()
 write_basic_package_version_file("${_package}ConfigVersion.cmake" COMPATIBILITY SameMajorVersion ARCH_INDEPENDENT)
 
 # Allow package maintainers to freely override the path for the configs
-set(ASIO_INSTALL_CMAKEDIR "${CMAKE_INSTALL_DATADIR}/${_package}"
+set(ASIO_INSTALL_CMAKEDIR "${CMAKE_INSTALL_LIBDIR}/cmake/${_package}"
     CACHE PATH "CMake package config location relative to the install prefix"
 )
 mark_as_advanced(ASIO_INSTALL_CMAKEDIR)
