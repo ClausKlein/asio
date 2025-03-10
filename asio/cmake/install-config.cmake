e9972499c2b178db585f715d3a6744800b24280b
ClausKlein <claus.klein@arcormail.de>
2025-03-10 10:50:10 +0100

    Use gersemi to formate cmake files

--- asio/cmake/install-config.cmake
+++ asio/cmake/install-config.cmake
@@ -3,28 +3,29 @@ find_dependency(Threads)
 find_dependency(OpenSSL)
 
 function(add_asio_module NAME)
-  set(ASIO_ROOT @CMAKE_INSTALL_PREFIX@)
-  message(STATUS "ASIO_ROOT is: ${ASIO_ROOT}")
+    set(ASIO_ROOT @CMAKE_INSTALL_PREFIX@)
+    message(STATUS "ASIO_ROOT is: ${ASIO_ROOT}")
 
-  add_library(${NAME})
-  target_include_directories(${NAME} PRIVATE ${ASIO_ROOT}/include)
-  target_compile_features(${NAME} PUBLIC cxx_std_23)
+    add_library(${NAME})
+    target_include_directories(${NAME} PRIVATE ${ASIO_ROOT}/include)
+    target_compile_features(${NAME} PUBLIC cxx_std_23)
 
-  set(CPPdefinitions "@CPPdefinitions@")
-  if(CPPdefinitions)
-    target_compile_definitions(${NAME} PUBLIC ${CPPdefinitions})
-  endif()
+    set(CPPdefinitions "@CPPdefinitions@")
+    if(CPPdefinitions)
+        target_compile_definitions(${NAME} PUBLIC ${CPPdefinitions})
+    endif()
 
-  # cmake-format: off
-    target_sources(${NAME} PUBLIC
-        FILE_SET modules_public
-        TYPE CXX_MODULES
-        BASE_DIRS ${ASIO_ROOT}
-        FILES
-            ${ASIO_ROOT}/lib/cmake/asio/module/asio.cppm
+    # cmake-format: off
+    target_sources(
+        ${NAME}
+        PUBLIC
+            FILE_SET modules_public
+            TYPE CXX_MODULES
+            BASE_DIRS ${ASIO_ROOT}
+            FILES ${ASIO_ROOT}/lib/cmake/asio/module/asio.cppm
     )
     # cmake-format: on
-  target_link_libraries(${NAME} PUBLIC OpenSSL::SSL OpenSSL::Crypto)
+    target_link_libraries(${NAME} PUBLIC OpenSSL::SSL OpenSSL::Crypto)
 endfunction()
 
 include("${CMAKE_CURRENT_LIST_DIR}/asioTargets.cmake")
